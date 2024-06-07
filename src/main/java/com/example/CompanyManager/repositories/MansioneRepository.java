cc6ccb53ce7939d1e8022816506f14de4a8fc428
Marco <167513418+M4rco592@users.noreply.github.com>
2024-06-07 15:18:38 +0200

    Update MansioneRepository.java

--- src/main/java/com/example/CompanyManager/repositories/MansioneRepository.java
+++ src/main/java/com/example/CompanyManager/repositories/MansioneRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MansioneRepository extends JpaRepository<MansioneRepository, Long> {
+public interface MansioneRepository extends JpaRepository<MansioneEntity, Long> {
 }
